a45f7db3b3d2163971f97a71c5f46de68ac6b786
shubham girase <sgirase08@gmail.com>
2021-11-03 14:43:30 +0530

    restyled navbar

--- script.js
+++
@@ -1,7 +0,0 @@
-const navBurger = document.querySelector("#humburger");
-const mobileNav = document.querySelector("#mobile-nav");
-
-navBurger.addEventListener("click", () => {
-  navBurger.classList.toggle("is-active");
-  mobileNav.classList.toggle("is-active");
-});
