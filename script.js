365515db9f15dec281800b00d2f81edc8fcd90e0
shubham girase <sgirase08@gmail.com>
2023-01-29 16:33:02 +0530

    fix aboutus marging bottom

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const currentYear = document.getElementById("currentYear");
 document.addEventListener("DOMContentLoaded", () => {
   const year = new Date().getFullYear();
-
   currentYear.innerText = year;
 });
